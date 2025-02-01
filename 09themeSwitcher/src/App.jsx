7be404c62c57eda80e9ae2f05498dc5d570b23c6
izhar <i4izharali9838@gmail.com>
2025-02-01 18:04:58 +0530

    themeSwitcher

---
+++ 09themeSwitcher/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+import useTheme, { ThemeProvider } from './contexts/Theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
+import { useEffect } from 'react'
+
+function App() {
+
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+
+  return (
+   <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+      <ThemeBtn/>
+      <Card/>
+   </ThemeProvider>
+  )
+}
+
+export default App
